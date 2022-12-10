03893162193baaea2991ad366387e9cb4703c831
loicsolbes <loic.solbes@gmail.com>
2022-12-10 19:02:33 +0100

    POST feature

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+//Server code
+
+const { request, response } = require('express');
 const express = require('express');
-
 const app = express()
-
 app.listen(3000, () => console.log('listening at 3000'))
-
 app.use(express.static('public'))
+app.use(express.json( {limit: '1mb'} ))
+
+app.post('/api', (request, response) => {
+    console.log(request.body)
+})
