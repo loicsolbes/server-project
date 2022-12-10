0a2dcd45b00be2f90707cf19e9481fde17f6ffe4
loicsolbes <loic.solbes@gmail.com>
2022-12-10 21:09:58 +0100

    database version

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 //Server code
 
 const { request, response } = require('express');
+
 const express = require('express');
+const Datastore = require('nedb')
+
 const app = express()
+
 app.listen(3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
 app.use(express.json( {limit: '1mb'} ))
 
+const dataBase = new Datastore('database.db')
+dataBase.loadDatabase()
+
 app.post('/api', (request, response) => {
-    console.log(request.body)
-})
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    dataBase.insert(data);
+    response.json(data);
+  });
