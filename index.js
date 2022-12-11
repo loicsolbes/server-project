c3b15ac9bb131529eed6ef7e98bc3d3663e2363f
loicsolbes <loic.solbes@gmail.com>
2022-12-11 18:30:03 +0100

    final version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 //const { request, response } = require('express');
 
+const { request, response } = require('express');
 const express = require('express');
 const Datastore = require('nedb')
 
@@ -14,6 +15,23 @@ app.use(express.json( {limit: '1mb'} ))
 const dataBase = new Datastore('database.db')
 dataBase.loadDatabase()
 
+
+
+app.get('/api', (request, response) => {
+
+  dataBase.find({}, (error, data) => {
+    if (error) {
+      response.end()
+      return
+    }
+    response.json(data)
+  })
+
+})
+
+
+
+// The code below handle the POST request made from the client side
 app.post('/api', (request, response) => {
     const data = request.body;
     const timestamp = Date.now();
